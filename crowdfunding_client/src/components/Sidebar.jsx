91c18b4e169b9682f75f5a137b3eb161b5098c6c
aadish-nityom <aadish-fr@pord.niyom.com>
2024-12-23 06:03:43 +0530

    add

--- crowdfunding_client/src/components/Sidebar.jsx
+++ crowdfunding_client/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import sun from '../assets/sun.svg';
-import { navlinks } from '../Constant/index';
+import { navlinks } from './constants';
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
     {!isActive ? (
